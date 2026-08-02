35e474e3314b5ef9566c46c4cf18e96cb566dae3
hemeshKas <srinik1305@gmail.com>
2026-08-02 16:08:19 +0530

    Remove lib, components, types to add as submodules

--- components/answer/StreamingCursor.tsx
+++
@@ -1,8 +0,0 @@
-export function StreamingCursor() {
-  return (
-    <span
-      aria-hidden
-      className="ml-0.5 inline-block h-[1.05em] w-[2px] translate-y-[2px] animate-blink bg-incogni-accent align-middle"
-    />
-  );
-}
